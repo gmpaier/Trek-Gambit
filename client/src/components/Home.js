775c713fe140fca43b820334a0338d5d631b9af0
Sean Monaghan <smonagha@conncoll.edu>
2021-06-19 13:18:11 -0400

    added routing for incoming pages

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "./Banner/Banner";
+import Container from "./Container/Container";
+
+function Home() {
+    return (
+    <Container >
+        <Banner />
+        {/* <PostDiv >
+            <Post />
+        </PostDiv> */}
+    </Container>
+    )
+}
+
+export default Home;
