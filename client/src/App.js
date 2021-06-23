2e05b0821f58579f9302a6ae3c490af41052c53a
Sean Monaghan <smonagha@conncoll.edu>
2021-06-23 19:15:25 -0400

    added a search page html and route structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react"
 import {BrowserRouter, Route, Switch } from "react-router-dom"
 import './App.css';
-import Home from "./components/Home";
+import Home from "./pages/Home";
 // import login from "./components/Login.js"
 // import Random from "./components/Random";
-// import Search from "./components/Search";
+import Search from "./pages/Search";
 // import Park from "./components/Park";
 // import Compare from "./components/Compare";
 // import Leaderboard from "./components/Leaderboard";
@@ -17,7 +17,7 @@ function App() {
         <Route path = "/" component = {Home} exact/>
         {/* <Route path = "/login" component = {Login} /> */}
         {/* <Route path = "/random" component = {Random} /> */}
-        {/* <Route path = "/search" component = {Search} /> */}
+        <Route path = "/search" component = {Search} />
         {/* <Route path = "/park" component = {Park} /> */}
         {/* <Route path = "/compare" component = {ParkOrNot} />  */}
         {/* <Route path = "/leaderboard" component = {Leaderboard} */}
