66698cc655e1559b79314273b6a19d87fdabc54b
Sean Monaghan <smonagha@conncoll.edu>
2021-06-25 14:59:56 -0400

    set up axios get request to search for park by name of park

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,26 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export default {
-//   // Gets all books
-//   getBooks: function() {
-//     return axios.get("/api/books");
-//   },
-//   // Gets the book with the given id
-//   getBook: function(id) {
-//     return axios.get("/api/books/" + id);
-//   },
-//   // Deletes the book with the given id
-//   deleteBook: function(id) {
-//     return axios.delete("/api/books/" + id);
-//   },
-//   // Saves a book to the database
-//   saveBook: function(bookData) {
-//     return axios.post("/api/books", bookData);
-//   }
-// };
+export default {
+
+  // Get park from nps search 
+  getNationalPark: function(query) {
+    return axios.get("https://developer.nps.gov/api/v1/parks?q=" + query +  "&api_key=1y0zIuzR0jebuL5GGN8LUNA07CshBja9C5SBzN3F")
+  },
+
+  // Gets all books
+  getParks: function() {
+    return axios.get("/api/parks");
+  },
+  // Gets the book with the given id
+  getPark: function(id) {
+    return axios.get("/api/parks/" + id);
+  },
+  // Deletes the book with the given id
+  deletePark: function(id) {
+    return axios.delete("/api/park/" + id);
+  },
+  // Saves a book to the database
+  savePark: function(bookData) {
+    return axios.post("/api/parks", bookData);
+  }
+};
