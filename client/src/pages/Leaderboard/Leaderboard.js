b613587bd6b4bd62affc1cd9b6cd9aa33adccdfa
Jarrete Barnett <jarretebarnett@gmail.com>
2021-07-07 21:33:27 -0400

    Pushing leaderboard progress.

--- client/src/pages/Leaderboard/Leaderboard.js
+++ client/src/pages/Leaderboard/Leaderboard.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 import { Container } from "../../components/Grid";
-import LeadTable from "../../components/LeadTable/LeadTable";
-// import axios from "axios";
+import LeadTable from "../../components/LeadTable";
 
 class Leaderboard extends Component {
 
+    state = {
+        data: {}
+    }
+
+    componentDidMount() {
+        API.getCountByPark().then(res => {
+            if(res.data.data === "error") {
+                throw new Error(res.data.data);
+            } else {
+                let results = res.data.data;
+                this.setState({data: results});
+            }
+        })
+        .catch(err => console.log(err));
+    }
 
     render() {
         return (
             <Container fluid>
-                <LeadTable />
+                <LeadTable
+                name = {this.state.name}
+                visits = {this.state.visits} />
             </Container>
         )
     }
